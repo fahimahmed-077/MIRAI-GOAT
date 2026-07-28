18725e9baae39bcea2d92c2ddb039a53dbecf69c
FAHIM AHMED RAJ <fahimahmedraj207@gmail.com>
2026-07-28 19:10:17 +0600

    Update auto.js

--- Script/commands/auto.js
+++ Script/commands/auto.js
@@ -54,7 +54,7 @@ module.exports = {
       // Send video with platform and title
       api.sendMessage(
         {
-          body: `🔰𝗥𝗮𝗵𝗮𝘁_𝗕𝗼𝘁🔰\n⚡𝗔𝘂𝘁𝗼 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗲𝗿⚡\n📍 Platform: ${site}`,
+          body: `⚡𝗙𝗔𝗛𝗜𝗠_𝗕𝗢𝗧⚡\n⚡𝗔𝘂𝘁𝗼 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗲𝗿⚡\n📍 Platform: ${site}`,
           attachment: fs.createReadStream(filePath),
         },
         event.threadID,
