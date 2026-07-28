9a5cc45c3448eebb7884ecdcd79489dfcbd3959b
FAHIM AHMED RAJ <fahimahmedraj207@gmail.com>
2026-07-28 19:36:01 +0600

    Update info.js

--- Script/commands/info.js
+++ Script/commands/info.js
@@ -22,14 +22,14 @@ module.exports.run = async function({ api, event }) {
 ┏━━━━━━━━━━━━━━━━┓
 ┃  🌟𝐎𝐖𝐍𝐄𝐑 𝐈𝐍𝐅𝐎🌟
 ┣━━━━━━━━━━━━━━━━┫
-┃👤𝐍𝐀𝐌𝐄      :𝐑𝐀𝐇𝐀𝐓
+┃👤𝐍𝐀𝐌𝐄      :𝗙𝗔𝗛𝗜𝗠
 ┃🚹𝐆𝐄𝐍𝐃𝐄𝐑    :𝐌𝐀𝐋𝐄
 ┃🎂𝐀𝐆𝐄       :𝟏𝟔
 ┃🕌𝐑𝐄𝐋𝐈𝐆𝐈𝐎𝐍 : 𝐈𝐒𝐋𝐀𝐌
-┃🏫𝐄𝐃𝐔𝐂𝐀𝐓𝐈𝐎𝐍 :বয়ড়া ইসরাইল
-┃🏡𝐀𝐃𝐃𝐑𝐄𝐒𝐒 :জামালপুর,বাংলাদেশ
+┃🏫𝐄𝐃𝐔𝐂𝐀𝐓𝐈𝐎𝐍 :🙂🙂
+┃🏡𝐀𝐃𝐃𝐑𝐄𝐒𝐒 :সিরাজগঞ্জ,বাংলাদেশ
 ┣━━━━━━━━━━━━━━━━┫
-┃𝐓𝐈𝐊𝐓𝐎𝐊 :@where.is.she15
+┃𝐓𝐈𝐊𝐓𝐎𝐊 :@tomgo.soto.bhaiya
 ┃📢𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌 :দিবো না🥴🤪
 ┃🌐𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 :বায়ো-তে আছে
 ┣━━━━━━━━━━━━━━━━┫
@@ -38,7 +38,7 @@ module.exports.run = async function({ api, event }) {
  attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/owner.jpg"));
 
- return request("https://i.imgur.com/FJI61jS.jpeg")
+ return request("https://i.imgur.com/D5ysSeC.jpeg")
  .pipe(fs.createWriteStream(__dirname + '/cache/owner.jpg'))
  .on('close', () => callback());
 };
